8c5606193309dc3feb41f3ff328e270e5b5939d2
davidjwp <93434152+davidjwp@users.noreply.github.com>
2022-12-03 22:40:05 +0100

    update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -10,29 +10,77 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include "libft.h"
+# include <stdarg.h>
+# include "libfp.h"
 
-int	ft_printf(const char *format, ...)
+void  ft_putchar_fd(char c, int fd)
 {
-	va_list	args;
-
-	char	* str;
-
-	str = (char *)format;
-	va_start(args, format);
-	while (str)
-	{
-		str = (char *)va_arg(args, const char *);
-		ft_putstr_fd(str, 1);
-	}
-	return (0);
-
-
+  write (fd, &c, 1);
 }
 
-int	main(int argc, char **argv)
+void  ft_putstr_fd(char *s, int fd)
 {
-	ft_printf ("hey");
+  while (s++)
+    ft_putchar_fd( *s, fd);
+}
+
+void	ft_cspd_spec(va_list _valist, char spec)
+{
+	switch (&spec)
+	{
+		case 'c':
+			ft_putchar_fd((char)va_arg( _valist, const char *), 1);
+			break;
+		case 's':
+			ft_putstr_fd((char *)va_arg( _valist, const char *), 1);
+			break;
+		case 'p':
+
+		case 'd':
+	}
+}
+
+void	ft_iuxX_spec(va_list _valist, char spec)
+{
+	switch (&spec)
+	{
+		case 'i';
+		case 'u';
+		case 'x';
+		case 'x';
+	}
+}
+
+int ft_printf(const char *format,...)
+{
+	va_list _valist;
+	char  *vastr;
+
+	vastr = (char *)format;
+	va_start( _valist, format);
+	while (*vastr)
+	{
+		if (*vastr == '%')
+		{
+			if (*vastr + 1 == ('c' || 's' || 'p' || 'd'))
+				ft_cspd_spec( _valist, vastr);
+			else if (*vastr + 1 == ('i' || 'u' || 'x' || 'X'))
+				ft_iuxX_spec( _valist, vastr);
+			else if (*vastr + 1 == '%')
+				ft_putchar_fd( '%', fd);
+			vastr + 2;
+		}
+		ft_putchar_fd( *vastr, fd);
+		vastr++;
+	}
 	return (0);
+}
+
+int main() 
+{
+	char  c;
+	c = 'c';
+	ft_printf("here is a char :%c",c);
+	"cspdiuxX%"
+	return 0;
 }
